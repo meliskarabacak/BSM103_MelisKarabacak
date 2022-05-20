427a5a7fddbfd32e668be6afbd7bb760afbbb0cc
meliskarabacak <91959805+meliskarabacak@users.noreply.github.com>
2022-05-20 16:08:19 +0300

    BaharDonemHafta12
    BaharDonemHafta12

---
+++ Bahar2022/BaharDonemHafta12/src/Uygulamalar/palindromeStack.java
@@ -0,0 +1,30 @@
+package Uygulamalar;
+import java.util.*;
+public class palindromeStack {
+
+	public static void main(String[] args) {
+	System.out.println("String giriniz:");
+	Scanner input= new Scanner (System.in);
+	String inputString=input.nextLine();
+	Stack stack = new Stack();
+	for ( int index =0; index<inputString.length(); index++) {
+		stack.push(inputString.charAt(index));
+	}
+	String reverseString="" ;
+	while (!stack.isEmpty()) {
+		reverseString = reverseString+stack.pop();
+		
+	}
+		if (inputString.equals(reverseString)) {
+			System.out.println("Girilen kelime palindromdur");
+			
+		}
+		else {
+			System.out.println("Girilen kelime palindrom degildir");
+			
+		}
+	
+
+	}
+
+}
